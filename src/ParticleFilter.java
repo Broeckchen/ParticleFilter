66f14f465ba37090f6b9bbb219890a647f34e17a
Barbara Schwarzwald <broeckschen@googlemail.com>
2012-11-27 00:04:53 +0100

    Comments

--- src/ParticleFilter.java
+++ src/ParticleFilter.java
@@ -13,6 +13,7 @@ import org.apache.commons.math3.distribution.NormalDistribution;
 
 public class ParticleFilter {
 
+	// parameters for the sensor and motion model
 	private static final double RELIABILITY = 0.9;
 	private static final double STANDARD_DEVIATION = 0.5;
 	private static final double MOVE_DEVIATION_FACTOR = 0.25;
@@ -20,10 +21,12 @@ public class ParticleFilter {
 			STANDARD_DEVIATION);
 	private static final double[] doors = { 1.0, 2.5, 5, 7 };
 
+	// parameters for the visualisation
 	private static final double ZOOM_FACTOR = 0.3;
 	private static final int PIC_HEIGHT = 100;
 	private static final double PIC_WIDTH_FACTOR = 10;
 
+	// member variables
 	private int step;
 	private List<Particle> particles;
 	private int amount;
@@ -35,6 +38,13 @@ public class ParticleFilter {
 		step = -1;
 	}
 
+	/*
+	 * Initializes the Particle Filter with amount particles uniformly
+	 * distributed between rangeStart and rangeEnd with equal weight of
+	 * 1/amount.
+	 * 
+	 * In case of the assignment, rangeStart=0, rangeEnd=10 and amount=100.
+	 */
 	public void initialize(double rangeStart, double rangeEnd, int amount) {
 		step = 0;
 		particles.clear();
@@ -48,6 +58,16 @@ public class ParticleFilter {
 		this.rangeEnd = rangeEnd;
 	}
 
+	/*
+	 * This function reweights all particles according to the sensor model.
+	 * 
+	 * The function used to reweight the particles is based on the distance x to
+	 * the closest door: 0.9*p(z|x) + 0.1*p(-z|x), using p(-z|x) = 1-p(z|x),
+	 * while p(z|x) is normally distributed.
+	 * 
+	 * After all particles have been reweighted according to that function, the
+	 * weights of all particles are normalized to summ up to 1.
+	 */
 	public void sensorStep(boolean doorDetected) {
 		step++;
 		if (doorDetected) {
@@ -61,28 +81,17 @@ public class ParticleFilter {
 				p.setWeight(newWeight);
 				normalisationQuotient += newWeight;
 			}
-			System.out.println(normalisationQuotient);
 			for (Particle p : particles) {
 				p.setWeight(p.getWeight() / normalisationQuotient);
 			}
 		} else {
-			double normalisationQuotient = 0.0;
-			for (Particle p : particles) {
-				double distance = findDoorDistance(p.getPosition());
-				double prior = PRIOR.density(distance);
-				double newWeight = p.getWeight()
-						* (RELIABILITY * (1 - prior) + (1 - RELIABILITY)
-								* prior);
-				p.setWeight(newWeight);
-				normalisationQuotient += newWeight;
-			}
-			System.out.println(normalisationQuotient);
-			for (Particle p : particles) {
-				p.setWeight(p.getWeight() / normalisationQuotient);
-			}
+			// not needed for the assignment
 		}
 	}
 
+	/*
+	 * This function calculates the distance to the closest door.
+	 */
 	public double findDoorDistance(double position) {
 		double minDistance = rangeEnd;
 		for (double doorPosition : doors) {
@@ -94,11 +103,27 @@ public class ParticleFilter {
 		return minDistance;
 	}
 
-	// done using systematic resampling
-	public void moveStep(double movement) {
+	/*
+	 * This function resamples the particles according to the motion model and
+	 * the distribution given by the weights of the particles.
+	 * 
+	 * Using systematic resampling amount (100 in the assignment) particles are
+	 * chosen according to their weight (particles with high weights might be
+	 * chosen multiple times, particles with low weight on the other hand
+	 * never). Then those particles are moved and disturbed according to the
+	 * motion command. The resulting particles are uniformly weighted and make
+	 * up the new particle set.
+	 * 
+	 * If a particles moves out of the given range (0 - 10 in the assignment) it
+	 * is discarded. If at the end of the resampling there aren't enough
+	 * particles, the missing number of particles is resampled again from the
+	 * complete distribution. This is repeated until there are exactly as many
+	 * particles as before.
+	 */
+	public void moveStep(double motion) {
 		step++;
-		NormalDistribution moveDistribution = new NormalDistribution(movement,
-				Math.abs(MOVE_DEVIATION_FACTOR * movement));
+		NormalDistribution moveDistribution = new NormalDistribution(motion,
+				Math.abs(MOVE_DEVIATION_FACTOR * motion));
 		List<Particle> resampledParticles = new ArrayList<Particle>();
 		Random r = new Random();
 		while (resampledParticles.size() < amount) {
@@ -124,6 +149,23 @@ public class ParticleFilter {
 		particles = resampledParticles;
 	}
 
+	/*
+	 * This function creates two files showing the current particles and their
+	 * weights. One is a picture with a visualization of the particles as
+	 * vertical black lines with a length according to their weight and a width
+	 * of exactly 1 pixel, the other is a text file containing weight and
+	 * position of all particles.
+	 * 
+	 * Note, that the size and scaling of the picture depends on the number of
+	 * particles and some parameters given at the beginning of this file. Note
+	 * that some particle lines might not fit completely into the picture if the
+	 * parameters are chosen poorly. It might also occur, that some particles
+	 * are not visible, if they are so close to another particle, that they fall
+	 * into the same pixel line.
+	 * 
+	 * The files are automatically named to not overwrite previous files.
+	 */
+
 	public void saveVisualisation() {
 		if (step > -1) {
 			int runId = 1;
@@ -136,6 +178,7 @@ public class ParticleFilter {
 						+ runId + "_step" + step + ".png");
 			}
 
+			// creation of an empty white picture of the correct bounds
 			BufferedImage img = new BufferedImage((int) (PIC_WIDTH_FACTOR
 					* amount + 1), PIC_HEIGHT + 1, BufferedImage.TYPE_BYTE_GRAY);
 			for (int i = 0; i < PIC_HEIGHT + 1; i++) {
@@ -143,6 +186,8 @@ public class ParticleFilter {
 					img.setRGB(j, i, Color.WHITE.getRGB());
 				}
 			}
+
+			// drawing the black line for each particles
 			for (Particle p : particles) {
 				int position = (int) Math.floor(p.getPosition()
 						* PIC_WIDTH_FACTOR * amount / (rangeEnd - rangeStart));
@@ -157,8 +202,8 @@ public class ParticleFilter {
 				textOutput = new FileWriter("output" + File.separator
 						+ "Particles_run" + runId + "_step" + step + ".txt");
 				for (Particle p : particles) {
-					textOutput.write("position=" + p.getPosition() + "\tweight="
-							+ p.getWeight() + "\r\n");
+					textOutput.write("position=" + p.getPosition()
+							+ "\tweight=" + p.getWeight() + "\r\n");
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
